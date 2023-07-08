106db980ac9bfe0fd613e75117f2d3c3ef4a26a9
Руслан <rus_boy2002@ukr.net>
2023-07-08 23:48:30 +0300

    upd01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+// const sss = document.querySelector()
 console.log(galleryItems);
