fe6c02d53cbb2bab78c1000f9898994f8d2e89b3
Ruslan0920 <rus_boy2002@ukr.net>
2023-07-10 15:09:12 +0300

    From terminal

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-// const sss = document.querySelector()
+
 console.log(galleryItems);
+
+const fullGallery = document.querySelector('.gallery');
+
+const createFullGallery = galleryItems.map(({ preview, original, description }) =>
+    `<li class = "gallery__item">
+    <a class = "gallery__link" href="${original}">
+    <img class = "gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}">
+    </a>
+    </li>`
+);
+
+fullGallery.insertAdjacentHTML('beforeend', createFullGallery.join(" "));
+fullGallery.addEventListener('click', onClick);
+
+function onClick(event) {
+    event.preventDefault();
+    const newTarget = event.target;
+
+    if (!newTarget.classList.contains('gallery__image')) {
+        return;
+    }
+}
