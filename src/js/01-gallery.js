a400419ed6cf2a02a116d805484b1476c3abc906
Руслан <rus_boy2002@ukr.net>
2023-07-12 23:56:01 +0300

    upd-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,7 +5,7 @@ import { galleryItems } from './gallery-items';
 console.log(galleryItems);
 
 import SimpleLightbox from "simplelightbox";
-// console.log(SimpleLightbox);
+console.log(SimpleLightbox);
 
 import "simplelightbox/dist/simple-lightbox.min.css";
 
@@ -23,8 +23,17 @@ const createFullGallery = galleryItems.map(({ preview, original, description }) 
 
 fullGallery.insertAdjacentHTML('beforeend', createFullGallery.join(" "));
 
+function onClick(event) {
+    event.preventDefault();
+    const newTarget = event.target;
+
+    if (!newTarget.classList.contains('gallery__image')) {
+        return;
+    }
+}
+
 let gallery = new SimpleLightbox('.gallery a');
-gallery.on('show.simplelightbox');
+gallery.on('show.simplelightbox', 'captionsData: alt', 'captionPosition: bottom', 'captionDelay: 250');
 
 // { captionPosition: "bottom", captionsData:"alt", captionDelay: 250}
 
