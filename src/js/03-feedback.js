d2d8424b59c4ee07b4e7975309fb3bdd747843a8
Руслан <rus_boy2002@ukr.net>
2023-07-27 21:35:42 +0300

    upd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,31 +5,28 @@ const refs = {
     email: document.querySelector('input'),
     textarea : document.querySelector('textarea')
 }
-console.log(refs.form);
-console.log(refs.email);
-console.log(refs.textarea);
+// console.log(refs.form);
+// console.log(refs.email);
+// console.log(refs.textarea);
 
-
+const objectLocalStorage = {};
+ console.log(objectLocalStorage);
 
 // refs.form.addEventListener('submit', onFormSubmit);
 // refs.email.addEventListener('input', onTextAreaInput);
 // refs.textarea.addEventListener('input', onTextAreaInput);
 refs.form.addEventListener('input', onTextAreaInput);
-console.log(form);
+// console.log(form);
 
- const objectLocalStorage = {};
+
 
 function onTextAreaInput(evt) {
-    // const message = evt.target.value;
-    console.log(evt);
-    // objectLocalStorage.email = refs.email.value;
-    // objectLocalStorage.message = refs.textarea.value;
+    // console.log(evt);
 
-    // console.log(objectLocalStorage);
-    // console.log(objectLocalStorage.message);
     console.log(refs.email.value);
     console.log(refs.textarea.value);
-    refs.email.value = objectLocalStorage.mail;
-    refs.textarea.value = objectLocalStorage.message;
-    // localStorage.setItem("feedback-form-state", message)
+    objectLocalStorage.mail = refs.email.value;
+    objectLocalStorage.message = refs.textarea.value;
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(objectLocalStorage))
 }
