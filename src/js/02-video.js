9dd7fbcf26657cc570c9307fb94b44e5edb58e79
Руслан <rus_boy2002@ukr.net>
2023-07-29 23:59:22 +0300

    first commit 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
 
+
+// console.log(vimeo);
+console.log("HELLO WORLD");
+
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
+
+
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Vimeo.Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+console.log(onPlay);
