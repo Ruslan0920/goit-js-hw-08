8862a91366766b2f14a88420d4b8ec6953fceec7
Руслан <rus_boy2002@ukr.net>
2023-07-30 00:25:07 +0300

    02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,25 +5,25 @@ import Player from '@vimeo/player';
 console.log("HELLO WORLD");
 
 const player = new Player('handstick', {
-    id: 19231868,
+    id: "vimeo-player",
     width: 640
 });
+console.log(player);
 
+// const iframe = document.querySelector('iframe');
+// const iframePlayer = new Vimeo.Player(iframe);
 
-const iframe = document.querySelector('iframe');
-const iframePlayer = new Vimeo.Player(iframe);
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
+// const onPlay = function(data) {
+//     // data is an object containing properties specific to that event
+// };
 
-const onPlay = function(data) {
-    // data is an object containing properties specific to that event
-};
-
-player.on('play', onPlay);
-console.log(onPlay);
+// player.on('play', onPlay);
+// console.log(onPlay);
