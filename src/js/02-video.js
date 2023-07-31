4213b74218c499951f22541b84c88fb79a5ad22d
Ruslan0920 <rus_boy2002@ukr.net>
2023-07-31 16:40:26 +0300

    upd 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,26 +1,35 @@
 import Player from '@vimeo/player';
+console.log(Player);
 
-
+const iframe = document.querySelector('iframe');
+// console.log(iframe);
+const player = new Player(iframe)
 // const player = new Player('handstick', {
 //     id: "vimeo-player",
 //     width: 640
 // });
-// console.log(player);
+console.log(player);
+// iframe.addEventListener()
+player.on('play', function (data) {
+    console.log(data);
+    console.log('played the video!');
+    const time = data.duration;
+    
+    console.log(time);
+});
+
+// const iframe = document.querySelector('iframe');
+// const iframePlayer = new Vimeo.Player(iframe);
 
 // player.on('play', function() {
 //     console.log('played the video!');
 // });
 
-const iframe = document.querySelector('iframe');
-const iframePlayer = new Vimeo.Player(iframe);
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
 
-player.on('play', function() {
-    console.log('played the video!');
-});
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
 
 // const onPlay = function(data) {
 //     // data is an object containing properties specific to that event
