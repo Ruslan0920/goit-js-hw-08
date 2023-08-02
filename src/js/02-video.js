01891f605e474dce2ef8bfd37b9ea04a0e4cf313
Ruslan0920 <rus_boy2002@ukr.net>
2023-08-02 16:37:14 +0300

    WTF 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,25 +1,47 @@
 import Player from '@vimeo/player';
-console.log(Player);
+// console.log(Player);
 
 const throttle = require("lodash.throttle");
+console.log(throttle);
 
 const LOCALSTORAGE_KEY = "videoplayer-current-time";
 
 const iframe = document.querySelector('iframe');
-// console.log(iframe);
+console.log(iframe);
 const player = new Player(iframe)
 console.log(player);
 
+// iframe.addEventListener('timeupdate', throttle(playVideo, 3000));
 
-const playVideo = player.on('timeupdate', function (data) {
+// function playVideo(e) {
+//     console.log(e);
+
+// }
+
+player.on('timeupdate', function (data) {
     // console.log(data);
-    const currentTime = data;
-    // throttle(currentTime, 3000)
-    console.log(currentTime);
-    // throttle((currentTime), 3000)
-    // console.log(currentTime);
     
-    // throttle(playVideo, 3000)
+    const currentTime = data;
+    console.log(currentTime);
 
-throttle(localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentTime)), 3000);
+    // throttle(currentTime, 3000)
+    // throttle(playVideo, 5000)
+    // console.log(playVideo);
+    
+    // throttle(() => {
+    //     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentTime));
+    // }, 3000)
+
+        // throttle((localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentTime))), 3000)
+
+    localStorage.setItem.throttle((LOCALSTORAGE_KEY, JSON.stringify(currentTime), 3000));
+    
 });
+
+// const input = document.querySelector(".js-inp")
+// console.log(input);
+// input.addEventListener('input', throttle(dataInput, 3000))
+
+// function dataInput(e) {
+//     console.log(e.target.value);
+// }
