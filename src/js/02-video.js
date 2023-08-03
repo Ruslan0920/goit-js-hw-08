6cc4531c3d8429694cd867adafdfab0ca766d2a8
Руслан <rus_boy2002@ukr.net>
2023-08-03 22:40:28 +0300

    upd 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -11,55 +11,24 @@ console.log(iframe);
 const player = new Player(iframe)
 console.log(player);
 
-// iframe.addEventListener('timeupdate', throttle(playVideo, 3000));
 
-// function playVideo(e) {
-//     console.log(e);
 
-// }
-// function playVideo (){
-
-// }
-
-    player.on('timeupdate', function (data) {
+player.on('timeupdate', function (data) {
     // console.log(data);
     
-    const currentTime = data;
-    console.log(currentTime);
+const currentTime = data;
+// console.log(currentTime);
 
-    // throttle(currentTime, 3000)
-    // throttle(playVideo, 5000)
-    // console.log(playVideo);
-    
-    // throttle(() => {
-    //     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentTime));
-    // }, 3000)
+localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentTime));
+});
 
-        // throttle((localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentTime))), 3000)
-
-    const local = localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentTime));
-
-//     player.getCurrentTime(currentTime).then(function(seconds) {
-//     // seconds = the actual time that the player seeked to
-// })
-player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY, JSON.stringify(currentTime))).then(function(seconds) {
+timeCurrent()
+        
+function timeCurrent() {
+const getTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    console.log(getTime.seconds);
+    player.setCurrentTime(getTime.seconds).then(function(seconds) {
     // seconds = the actual time that the player seeked to
-    seconds(100.000)
 })
-    });
-
-//     player.setCurrentTime().then(function(seconds) {
-//     // seconds = the actual time that the player seeked to
-// })
-
-// const input = document.querySelector(".js-inp")
-// console.log(input);
-// input.addEventListener('input', throttle(dataInput, 3000))
-
-// function dataInput(e) {
-//     console.log(e.target.value);
-// }
-
-// player.setCurrentTime(currentTime).then(function(seconds) {
-//     // seconds = the actual time that the player seeked to
-// })
+    
+}   
