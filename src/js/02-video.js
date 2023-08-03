83e015c72c863e0787d700d88f4aac0d5ae6cf70
Руслан <rus_boy2002@ukr.net>
2023-08-03 23:15:24 +0300

    update 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -11,16 +11,25 @@ console.log(iframe);
 const player = new Player(iframe)
 console.log(player);
 
+playVideo()
+console.log(throttle(playVideo, 5000));
 
 
-player.on('timeupdate', function (data) {
+function playVideo() {
+   player.on('timeupdate', function (data) {
     // console.log(data);
     
 const currentTime = data;
-// console.log(currentTime);
+       console.log(currentTime);
+       
+    //    function addToLocal() {}
+                //   throttle(addToLocal, 3000)
+       const add = localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentTime));
+    //    throttle(add, 3000)
+    //    console.log(add);
+}); 
+}
 
-localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentTime));
-});
 
 timeCurrent()
         
@@ -29,6 +38,7 @@ const getTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
     console.log(getTime.seconds);
     player.setCurrentTime(getTime.seconds).then(function(seconds) {
     // seconds = the actual time that the player seeked to
+        console.log(seconds);
 })
     
 }   
