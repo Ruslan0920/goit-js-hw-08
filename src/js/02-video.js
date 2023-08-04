3c5f3dcc95b767815823f4295f867f410bf6ec6e
Ruslan0920 <rus_boy2002@ukr.net>
2023-08-04 15:55:04 +0300

    02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,28 +7,29 @@ console.log(throttle);
 const LOCALSTORAGE_KEY = "videoplayer-current-time";
 
 const iframe = document.querySelector('iframe');
-console.log(iframe);
+// console.log(iframe);
 
-console.log(iframe);
 const player = new Player(iframe)
 console.log(player);
-// iframe.addEventListener('timeupdate', throttle(player.on, 3000))
+iframe.addEventListener('onplaying', throttle(playVideo, 3000));
+console.log(iframe);
 
 playVideo()
 
 function playVideo() {
    player.on('timeupdate', function (data) {
-    // console.log(data);
+       console.log(data);
     
 const currentTime = data;
-       console.log(currentTime);
- throttle(player.on, 3000)      
+    //    console.log(currentTime);
+   
     //    function addToLocal() {}
                 //   throttle(addToLocal, 3000)
 localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentTime));
     //    throttle(add, 3000)
     //    console.log(add);
     //    throttle(playVideo,3000)
+    //    throttle(localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentTime)), 3000)    
 }); 
 }
 
@@ -37,7 +38,7 @@ timeCurrent()
 function timeCurrent() {
 const getTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
     console.log(getTime.seconds);
-    player.setCurrentTime(getTime.seconds).then(function(seconds) {
+    player.setCurrentTime(getTime.seconds).then(function() {
     // seconds = the actual time that the player seeked to
         // console.log(seconds);
 })
