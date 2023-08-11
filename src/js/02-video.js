375a2d8626a64f75d8d19e32c320acdc10d2fcc6
Ruslan0920 <rus_boy2002@ukr.net>
2023-08-11 12:53:11 +0300

    02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,10 +8,11 @@ const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe)
 
-const onPlay = throttle(function(data) {
-    const timerVideo = data;
+const onPlay = throttle(function (data) {
+    console.log(data.seconds);
+    const timerVideo = data.seconds;
     // console.log(timerVideo);
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(timerVideo));    
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(timerVideo)) || {};    
 }, 1000);
 
 player.on('timeupdate', onPlay);
@@ -19,7 +20,7 @@ player.on('timeupdate', onPlay);
 timeCurrent()
         
 function timeCurrent() {
-const getTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-    // console.log(getTime.seconds);
-    player.setCurrentTime(getTime.seconds);
+const getTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY) || {});
+    console.log(getTime);
+    player.setCurrentTime(getTime);
 }   
